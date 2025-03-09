2bc5ff0afe27087d52bad6973e9765134a06038b
Drew <145530440+DrewRoss5@users.noreply.github.com>
2025-03-09 15:19:34 -0600

    Add a simple CLI interface
    This unofficially marks the first "true" build of TVM!

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,111 @@
 #include <iostream>
 #include <iomanip>
+#include <unordered_map>
 
 #include "../inc/assembler.h"
-#include "../inc/stack.hpp"
 #include "../inc/machine.h"
 
-template <typename T>
-std::string to_hex(T in){
-    std::stringstream ss;
-    ss << std::setw(sizeof(T)*2) << std::setfill('0') << std::hex << (int) in;
-    return ss.str();
+enum Command{
+    NULL_CMD,
+    HELP,
+    BUILD,
+    RUN
+};
+
+void print_error(const std::string& err_msg);
+Command parse_command(const std::string& command);
+void print_help();
+int assemble_prog(const std::string& in, const std::string& out);
+int exec_prog(const std::string& in);
+
+int main(int argc, char** argv){
+    if (argc == 1){
+        print_help();
+        return 0;
+    }
+    Command cmd = parse_command(argv[1]);
+    std::string in, out;
+    switch (cmd){
+        case NULL_CMD:
+            print_error("Unrecognized command: " + cmd);
+            print_help();
+            return -1;
+        case HELP:
+            print_help();
+            return 0;
+        case BUILD:
+            if (argc < 3 || argc > 4){
+                print_error("this command expects between one and two arguments. Use 'tvm help' for more information");
+                return 1;
+            }
+            in = argv[2];
+            out = "out.tcode";
+            if (argc == 4){
+                out = argv[3];
+                if (out.substr(out.size() - 7) == ".tcode")
+                    out.append(".tcode");
+            }
+            return assemble_prog(in, out);
+        case RUN:
+            if (argc != 3)
+                print_error("this command only accepts one argument. Use 'tvm help' for more information");
+            in = argv[2];
+            return exec_prog(in);
+    }
+    return 0;
 }
 
-void print_registers(Machine& vm){
-    for (int i = 0; i < 16; i++)
-        std::cout << "R" << i << ": " << vm.get_register(i) << "\n";
-    std::cout << std::endl;
+void print_error(const std::string& err_msg) {
+    std::cout << "\033[31mError:\033[0m " << err_msg << std::endl;
 }
 
-int main(){
-    Assembler assembler;
+Command parse_command(const std::string& command){
+    std::unordered_map<std::string, Command> options{
+        {"help", HELP},
+        {"build", BUILD},
+        {"run", RUN}
+    };
+    auto cmd_itt = options.find(command);
+    if (cmd_itt == options.end())
+        return NULL_CMD;
+    return cmd_itt->second;
+}
+
+void print_help(){
+    std::string names[] = {"help", "build", "run"};
+    std::string args[] = {"", "<input_file> [output_file]", "<input_file>"};
+    std::string descriptions[] = {
+        "displays this menu",
+        "assembles the input_file and stores the bytecode to the output file. If no output file is provided the bytecode will be stored in out.tcode",
+        "executes the provided tcode file"
+    };
+    std::cout << "Program options" << std::endl;
+    for (int i = 0; i < 3; i++)
+        std::cout << "\t" << std::left << std::setw(10) << names[i] << std::setw(35) << args[i] << descriptions[i] << "\n";
+    std::cout << "Visit https://github.com/DrewRoss5/TinkerVM for more information" << std::endl;
+}
+
+int assemble_prog(const std::string& in, const std::string& out){
+    try{
+        Assembler assembler; 
+        assembler.assemble_file(in, out);
+        std::cout << "Built " << out << " succesfully." << std::endl;
+    }
+    catch (std::runtime_error err){
+        print_error(err.what());
+        return -1;
+    }
+    return 0;
+}
+
+int exec_prog(const std::string& in){
     Machine vm;
-    assembler.assemble_file("../examples/functest.tasm", "func.tcode");
-    vm.exec_file("func.tcode");
-    print_registers(vm);
+    try{
+        vm.exec_file(in);
+    }
+    catch (std::runtime_error err){
+        print_error(err.what());
+        return -1;
+    }
     return 0;
 }
